62f97d4faf9aef7ae86fa987e8fef863937661b6
sepro <sepro@sepr0.com>
2025-10-17 23:22:28 +0200

    Only mock if necessary

--- src/yt/solver/setup.ts
+++ src/yt/solver/setup.ts
@@ -1,22 +1,36 @@
 import { parse } from "meriyah";
 
 export const setupNodes = parse(`
-globalThis.XMLHttpRequest = { prototype: {} };
-const window = Object.assign(Object.create(null), globalThis);
-window.location = {
-    hash: "",
-    host: "www.youtube.com",
-    hostname: "www.youtube.com",
-    href: "https://www.youtube.com/watch?v=yt-dlp-wins",
-    origin: "https://www.youtube.com",
-    password: "",
-    pathname: "/watch",
-    port: "",
-    protocol: "https:",
-    search: "?v=yt-dlp-wins",
-    username: "",
-};
-const document = {};
-const navigator = {};
-let self = globalThis;
+if (typeof globalThis.XMLHttpRequest === "undefined") {
+    globalThis.XMLHttpRequest = { prototype: {} };
+}
+if (typeof window === "undefined") {
+    var window = Object.create(null);
+}
+if (typeof URL === "undefined") {
+    window.location = {
+        hash: "",
+        host: "www.youtube.com",
+        hostname: "www.youtube.com",
+        href: "https://www.youtube.com/watch?v=yt-dlp-wins",
+        origin: "https://www.youtube.com",
+        password: "",
+        pathname: "/watch",
+        port: "",
+        protocol: "https:",
+        search: "?v=yt-dlp-wins",
+        username: "",
+    };
+} else {
+    window.location = new URL("https://www.youtube.com/watch?v=yt-dlp-wins");
+}
+if (typeof globalThis.document === "undefined") {
+    globalThis.document = Object.create(null);
+}
+if (typeof globalThis.navigator === "undefined") {
+    globalThis.navigator = Object.create(null);
+}
+if (typeof self === "undefined") {
+    var self = globalThis;
+}
 `).body;
