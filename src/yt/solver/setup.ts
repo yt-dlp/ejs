49dd27c3375b1ab8891e2b332fdb220a95849c70
sepro <sepro@sepr0.com>
2025-10-17 15:52:41 +0200

    Improve compatibility

--- src/yt/solver/setup.ts
+++ src/yt/solver/setup.ts
@@ -3,7 +3,20 @@ import { parse } from "meriyah";
 export const setupNodes = parse(`
 globalThis.XMLHttpRequest = { prototype: {} };
 const window = Object.assign(Object.create(null), globalThis);
-window.location = new URL("https://www.youtube.com/watch?v=yt-dlp-wins");
+window.location = {
+    hash: "",
+    host: "www.youtube.com",
+    hostname: "www.youtube.com",
+    href: "https://www.youtube.com/watch?v=yt-dlp-wins",
+    origin: "https://www.youtube.com",
+    password: "",
+    pathname: "/watch",
+    port: "",
+    protocol: "https:",
+    search: "?v=yt-dlp-wins",
+    username: "",
+};
 const document = {};
+const navigator = {};
 let self = globalThis;
 `).body;
