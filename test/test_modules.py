34a1e03ce8d453febbe35c271e52796b8efb51bc
sepro <sepro@sepr0.com>
2025-10-23 20:11:40 +0200

    Magic comma
    Co-authored-by: Simon Sawicki <contact@grub4k.dev>

--- test/test_modules.py
+++ test/test_modules.py
@@ -11,10 +11,12 @@ LIB_PATH = BASE_PATH / "yt_dlp_ejs/yt/solver/lib.min.js"
 class TestModules(unittest.TestCase):
     def test_yt_solver(self):
         self.assertEqual(
-            yt_dlp_ejs.yt.solver.core(), CORE_PATH.read_text(encoding="utf-8")
+            yt_dlp_ejs.yt.solver.core(),
+            CORE_PATH.read_text(encoding="utf-8"),
         )
         self.assertEqual(
-            yt_dlp_ejs.yt.solver.lib(), LIB_PATH.read_text(encoding="utf-8")
+            yt_dlp_ejs.yt.solver.lib(),
+            LIB_PATH.read_text(encoding="utf-8"),
         )
 
 
