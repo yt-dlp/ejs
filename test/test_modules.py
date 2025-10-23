f3fabf315f81ad3ad9a9cef66086fb5f96b428f3
bashonly <bashonly@protonmail.com>
2025-10-23 00:08:59 -0500

    what

--- test/test_modules.py
+++ test/test_modules.py
@@ -9,8 +9,10 @@ LIB_PATH = Path('yt_dlp_ejs/yt/solver/lib.min.js')
 
 class TestModules(unittest.TestCase):
     def test_yt_solver(self):
-        self.assertEqual(yt_dlp_ejs.yt.solver.core(), CORE_PATH.read_text(encoding='utf-8'))
-        self.assertEqual(yt_dlp_ejs.yt.solver.lib(), LIB_PATH.read_text(encoding='utf-8'))
+        self.assertEqual(yt_dlp_ejs.yt.solver.core(), 'core')
+        self.assertEqual(yt_dlp_ejs.yt.solver.lib(), 'lib')
+        # self.assertEqual(yt_dlp_ejs.yt.solver.core(), CORE_PATH.read_text(encoding='utf-8'))
+        # self.assertEqual(yt_dlp_ejs.yt.solver.lib(), LIB_PATH.read_text(encoding='utf-8'))
 
 
 if __name__ == '__main__':
