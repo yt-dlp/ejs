11c24a1fb5fd9f954aea3d7a6f476126f51f4339
bashonly <bashonly@protonmail.com>
2025-10-23 19:03:21 -0500

    Fix encoding for Windows

--- yt_dlp_ejs/yt/solver/__init__.py
+++ yt_dlp_ejs/yt/solver/__init__.py
@@ -7,11 +7,15 @@ def core() -> str:
     """
     Read the contents of the JavaScript core solver bundle as string.
     """
-    return (importlib.resources.files(yt_dlp_ejs.yt.solver) / "core.min.js").read_text()
+    return (importlib.resources.files(yt_dlp_ejs.yt.solver) / "core.min.js").read_text(
+        encoding="utf-8"
+    )
 
 
 def lib() -> str:
     """
     Read the contents of the JavaScript library solver bundle as string.
     """
-    return (importlib.resources.files(yt_dlp_ejs.yt.solver) / "lib.min.js").read_text()
+    return (importlib.resources.files(yt_dlp_ejs.yt.solver) / "lib.min.js").read_text(
+        encoding="utf-8"
+    )
