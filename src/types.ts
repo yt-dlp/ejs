129de85d7227dd417b1ca47be38d59fe830a5fcb
sepro <sepro@sepr0.com>
2025-10-28 18:42:49 +0100

    format

--- src/types.ts
+++ src/types.ts
@@ -1,14 +1,14 @@
 type DP<T> = T extends (infer U)[]
   ? DeepPartial<U>[]
   : T extends object
-  ? { [P in keyof T]?: DeepPartial<T[P]> }
-  : T;
+    ? { [P in keyof T]?: DeepPartial<T[P]> }
+    : T;
 
 type ValueOf<T> = T extends (infer U)[]
   ? U
   : T extends object
-  ? T[keyof T]
-  : never;
+    ? T[keyof T]
+    : never;
 
 export type DeepPartial<T> =
   | DP<T>
