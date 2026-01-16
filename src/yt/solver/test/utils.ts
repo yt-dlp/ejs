fc6613acdcc4a053dbd51be41477855f03dcbc10
Simon Sawicki <contact@grub4k.dev>
2026-01-16 17:37:22 +0100

    urgh, sometimes deno sucks

--- src/yt/solver/test/utils.ts
+++ src/yt/solver/test/utils.ts
@@ -1,6 +1,6 @@
 import { type Variant } from "./tests.ts";
 
-export const prefix = import.meta.dir + "/players";
+export const prefix = "src/yt/solver/test/players";
 
 export function getCachePath(player: string, variant: Variant) {
   return `${prefix}/${player}-${variant}`;
